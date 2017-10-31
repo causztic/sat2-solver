db41ad784aec3337b9466136c2f54346532ddb9b
yaojie <limyaojie93@gmail.com>
2017-11-01 00:37:18 +0800

    + another sat test file, update graph

--- src/sat/Graph.java
+++ src/sat/Graph.java
@@ -1,34 +1,20 @@
 package sat;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
-
-import immutable.*;
 import sat.formula.*;
 
 public class Graph {
 
-	// This class represents a directed graph using adjacency
-	// list representation
-//	private int V; // No. of vertices
 	private HashMap<Literal, LinkedList<Literal>> vertices = new HashMap<>();
-	//private LinkedList<Integer> adj[]; // Adjacency List
-
-//	// Constructor
-//	Graph(int v) {
-//		V = v;
-//		adj = new LinkedList[v];
-//		for (int i = 0; i < v; ++i)
-//			adj[i] = new LinkedList();
-//	}
-//
-//	// Function to add an edge into the graph
-//	void addEdge(int v, int w) {
-//		adj[v].add(w);
-//	}
-//	
+	private ArrayList<Literal> results = new ArrayList<>();
+	
+	ArrayList<Literal> getResults(){
+		return results;
+	}
 	void addEdge(Literal v, Literal w){
 		// link the literal v to the literal w.
 		LinkedList<Literal> edges = vertices.get(v);
@@ -38,25 +24,6 @@ public class Graph {
 		edges.add(w);
 		vertices.put(v, edges);
 	}
-
-	// A recursive function used by topologicalSort
-//	void topologicalSortUtil(int v, boolean visited[], Stack<Integer> stack) {
-//		// Mark the current node as visited.
-//		visited[v] = true;
-//		Integer i;
-//
-//		// Recur for all the vertices adjacent to this
-//		// vertex
-//		Iterator<Integer> it = adj[v].iterator();
-//		while (it.hasNext()) {
-//			i = it.next();
-//			if (!visited[i])
-//				topologicalSortUtil(i, visited, stack);
-//		}
-//
-//		// Push current vertex to stack which stores result
-//		stack.push(new Integer(v));
-//	}
 	
 	void topologicalSortUtil(Literal l, HashMap<Literal, Boolean> visited, Stack<Literal> stack){
 		// mark the node as visited.
@@ -94,30 +61,6 @@ public class Graph {
 		}
 		
 		while (!stack.isEmpty())
-			System.out.print(stack.pop() + " ");
-	}
-//	void topologicalSort() {
-//		Stack stack = new Stack();
-//
-//		// Mark all the vertices as not visited
-//		boolean visited[] = new boolean[V];
-//		for (int i = 0; i < V; i++)
-//			visited[i] = false;
-//
-//		// Call the recursive helper function to store
-//		// Topological Sort starting from all vertices
-//		// one by one
-//		for (int i = 0; i < V; i++)
-//			if (visited[i] == false)
-//				topologicalSortUtil(i, visited, stack);
-//
-//		// Print contents of stack
-//		while (stack.empty() == false)
-//			System.out.print(stack.pop() + " ");
-//	}
-
-	// Driver method
-	public static void main(String args[]) {
-
+			results.add(stack.pop());	
 	}
 }
