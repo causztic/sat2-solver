5dd0c47bfba23406c5d172d03fe6241e8fcaafc4
yaojie <limyaojie93@gmail.com>
2017-11-02 11:44:44 +0800

    refactor formulareader out

---
+++ src/sat/FormulaReader.java
@@ -0,0 +1,123 @@
+package sat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import sat.formula.Clause;
+import sat.formula.Formula;
+import sat.formula.Literal;
+import sat.formula.NegLiteral;
+import sat.formula.PosLiteral;
+
+public class FormulaReader {
+	private boolean is2SAT;
+	private Graph graph;
+	
+	public FormulaReader(){
+		is2SAT = true;
+		graph = new Graph();
+	}
+	
+
+	public boolean is2SAT() {
+		return is2SAT;
+	}
+	
+	public Graph getGraph(){
+		return graph;
+	}
+	
+	
+	public Formula readFile(String filename){
+		System.out.println("Reading file: " + filename);
+		boolean hasP = false;
+		Clause[] clauses = null;
+		int clausePointer = 0;
+		Scanner sc = null;
+
+		try {
+			sc = new Scanner(new File(filename));
+			String input = null;
+			
+			while (sc.hasNextLine()){
+				input = sc.nextLine();
+				// skip if empty or is a comment
+				if (input.isEmpty() || input.charAt(0) == 'c')
+					continue;
+				if (input.charAt(0) == 'p'){
+					String[] temp = input.split(" ");
+					clauses = new Clause[Integer.parseInt(temp[temp.length - 1])];
+					clausePointer = Integer.parseInt(temp[temp.length - 1]) - 1;
+					hasP = true;
+					break;
+				}
+					
+			}
+			
+			if (hasP){
+				sc.useDelimiter(" 0");
+				while (sc.hasNext()){
+					String next = sc.next();
+					String[] values = next.trim().split(" ");
+					if (values.length > 2){
+						// throw new IOException("The .cnf file has more than 2 literals in a clause.");
+						is2SAT = false;
+					}
+					Literal[] literals = new Literal[values.length];
+					for (int i = 0; i < literals.length; i++){
+						String temp = values[i].trim();
+						if (temp.length() > 0)
+							literals[i] = temp.charAt(0) == '-' ? NegLiteral.make(temp.substring(1)) : PosLiteral.make(temp);
+					}
+					
+					if (literals[0] != null){
+						clauses[clausePointer] = makeCl(literals);
+						if (clauses[clausePointer] == null)
+							throw new IOException("UNSATISFIABLE");
+						
+						// based on the current clause, construct the directed graph.
+						Iterator<Literal> it;
+						it = clauses[clausePointer].iterator();
+						if (it.hasNext()){
+							Literal firstItem = it.next();
+							Literal secondItem = it.hasNext() ? it.next() : firstItem;
+							graph.addEdge(firstItem.getNegation(), secondItem);
+							graph.addEdge(secondItem.getNegation(), firstItem);
+						}
+						clausePointer--;
+					}
+					
+
+				}
+			} else {
+				throw new IOException("invalid format for CNF. no P found.");
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e){
+			e.printStackTrace();
+		} finally {
+			sc.close();
+		}
+		return makeFm(clauses);
+	}
+	
+    private static Formula makeFm(Clause... e) {
+        Formula f = new Formula();
+        for (Clause c : e) {
+            f = f.addClause(c);
+        }
+        return f;
+    }
+    
+    private static Clause makeCl(Literal... e) {
+        Clause c = new Clause();
+        for (Literal l : e) {
+            c = c.add(l);
+        }
+        return c;
+    }
+}
--- src/sat/SATSolverTest.java
+++ src/sat/SATSolverTest.java
@@ -1,14 +1,5 @@
 package sat;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Scanner;
-
-import static org.junit.Assert.*;
-
-
 import sat.env.*;
 import sat.formula.*;
 
@@ -20,94 +11,21 @@ public class SATSolverTest {
     Literal na = a.getNegation();
     Literal nb = b.getNegation();
     Literal nc = c.getNegation();
-    
-    boolean is2SAT = true;
-	Graph g = new Graph();
+
 	
-	public Formula readFile(String filename){
-		boolean hasP = false;
-		Clause[] clauses = null;
-		int clausePointer = 0;
-		Scanner sc = null;
-
-		try {
-			sc = new Scanner(new File(filename));
-			String input = null;
-			
-			while (sc.hasNextLine()){
-				input = sc.nextLine();
-				// skip if empty or is a comment
-				if (input.isEmpty() || input.charAt(0) == 'c')
-					continue;
-				if (input.charAt(0) == 'p'){
-					String[] temp = input.split(" ");
-					clauses = new Clause[Integer.parseInt(temp[temp.length - 1])];
-					clausePointer = Integer.parseInt(temp[temp.length - 1]) - 1;
-					hasP = true;
-					break;
-				}
-					
-			}
-			
-			if (hasP){
-				sc.useDelimiter(" 0");
-				while (sc.hasNext()){
-					String next = sc.next();
-					String[] values = next.trim().split(" ");
-					if (values.length > 2){
-						// throw new IOException("The .cnf file has more than 2 literals in a clause.");
-						is2SAT = false;
-					}
-					Literal[] literals = new Literal[values.length];
-					for (int i = 0; i < literals.length; i++){
-						String temp = values[i].trim();
-						if (temp.length() > 0)
-							literals[i] = temp.charAt(0) == '-' ? NegLiteral.make(temp.substring(1)) : PosLiteral.make(temp);
-					}
-					
-					if (literals[0] != null){
-						clauses[clausePointer] = makeCl(literals);
-						if (clauses[clausePointer] == null)
-							throw new IOException("UNSATISFIABLE");
-						
-						// based on the current clause, construct the directed graph.
-						Iterator<Literal> it;
-						it = clauses[clausePointer].iterator();
-						if (it.hasNext()){
-							Literal firstItem = it.next();
-							Literal secondItem = it.hasNext() ? it.next() : firstItem;
-							g.addEdge(firstItem.getNegation(), secondItem);
-							g.addEdge(secondItem.getNegation(), firstItem);
-						}
-						clausePointer--;
-					}
-					
-
-				}
-			} else {
-				throw new IOException("invalid format for CNF. no P found.");
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e){
-			e.printStackTrace();
-		} finally {
-			sc.close();
-		}
-		return makeFm(clauses);
-	}
-	public void testTopological(){
+	public void testTopological(FormulaReader reader){
+		Graph graph = reader.getGraph();
 		System.out.println("SAT solver with topological sorting");
 		long started = System.nanoTime(); 
 		try {
-			g.topologicalSort();
+			graph.topologicalSort();
 		} catch(Exception e){
 			System.out.println(e);
 		} finally {
 			long time = System.nanoTime();
 			long timeTaken= time - started;
 			System.out.println("Time:" + timeTaken/1_000_000.0 + "ms");
-			System.out.println(g.getEnvironment());
+			System.out.println(graph.getEnvironment());
 		}
 	}
 	public void testDPLL(Formula f){
@@ -122,58 +40,21 @@ public class SATSolverTest {
 		} else 
 			System.out.println(e);
 	}
+	
 	public static void main(String[] args){
 		if (args.length > 0){
 	        String fileName = args[0];
 	        SATSolverTest r = new SATSolverTest();
-	        Formula f = r.readFile(fileName);
-	        if (r.is2SAT){
-	        	r.testTopological();
+	        FormulaReader reader = new FormulaReader();
+	        Formula f = reader.readFile(fileName);
+	        if (reader.is2SAT()){
+	        	r.testTopological(reader);
 	        } else {
 	        	r.testDPLL(f);
 	        }
-//	        r.testSATSolver1();
-//	        r.testSATSolver2();
 		} else {
 			System.out.println("Supply the location of the file. Syntax: solver.jar file");
 			System.exit(1);
 		}
 	}
-	
-    public void testSATSolver1(){
-    	// (a v b)
-    	Environment e = SATSolver.solve(makeFm(makeCl(a,b))	);
-    	System.out.println(e);	
-    	assertTrue( "one of the literals should be set to true",
-    			Bool.TRUE == e.get(a.getVariable())  
-    			|| Bool.TRUE == e.get(b.getVariable())	);
-    }
-    
-    
-    public void testSATSolver2(){
-    	// (~a)
-    	Environment e = SATSolver.solve(makeFm(makeCl(na)));
-    	System.out.println(e);
-    	assertEquals( Bool.FALSE, e.get(na.getVariable()));
-   
-    }
-    
-    private static Formula makeFm(Clause... e) {
-        Formula f = new Formula();
-        for (Clause c : e) {
-            f = f.addClause(c);
-        }
-        return f;
-    }
-    
-    private static Clause makeCl(Literal... e) {
-        Clause c = new Clause();
-        for (Literal l : e) {
-            c = c.add(l);
-        }
-        return c;
-    }
-    
-    
-    
 }
